1fe845adf76d28f19aa9b10f3861a641992dca50
shahar halutz <shahar.halutz@gmail.com>
2021-11-12 11:55:41 +0100

    initial commit (demo)

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name='amariltb',
+    version='0.0.3',
+    author='Shahar Halutz',
+    author_email='shahar.halutz@gmail.com',
+    description='provides amaril lab services',
+    url='https://github.com/Muls/toolbox',
+    project_urls = {
+        "Bug Tracker": "https://github.com/Muls/toolbox/issues"
+    },
+    license='MIT',
+    include_package_data=True,
+    packages=['amariltb'],
+    install_requires=['boto3'],
+    zip_safe=False,
+)
