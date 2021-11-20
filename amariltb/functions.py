86b9890a781dd23ad791a2796e3a516d65a74520
shahar halutz <shahar.halutz@gmail.com>
2021-11-20 21:11:39 +0100

    added rest of functions

--- amariltb/functions.py
+++ amariltb/functions.py
@@ -4,6 +4,7 @@ import codecs
 import json
 import pathlib
 from .pw_converter_db import DynamoDB
+from .pw_converter_utils import update_sheet
 from . import pw_converter as converter
 from . import audio_unifier_meta_merger as merger
 from . import audio_unifier as wav_unifier
@@ -11,7 +12,7 @@ from . import audio_unifier as wav_unifier
 import boto3
 
 
-def init_DB(config_path = None):
+def init_DB(version,config_path = None):
     """Amaril CLI offers the following commands: (for help type: amaril_cli COMMAND --help)"""
     
     # CONFIG:
@@ -43,29 +44,27 @@ def init_DB(config_path = None):
                             aws_access_key_id= os.environ['AWS_ACCESS_KEY_ID'],
                             aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'],
                             region_name=os.environ['REGION_NAME'] )
+    DynamoDB.version = version
 
 """unifies wave files from a directory in the cloud, to chunks localy."""
 def unify_waves(bucket_name, source_dir,target_dir,files_per_chunk):
-    init_DB()
+    # TBD: separate google config out of init_DB:
+    init_DB("TEST")
     wav_unifier.unify(None,bucket_name,source_dir,target_dir,files_per_chunk)
 
-
 def get_index(language,category,target_filename,version):    
-    init_DB()
-    DynamoDB.version = version
+    init_DB(version)
     converter.handle_get_index(language,category,target_filename)
 
 """merges sonix product and meta files, and processes into the ProductWords data base table."""
 def process_sonix(input_dir,target_filename, language,category,version):
-    init_DB()
-    DynamoDB.version = version
+    init_DB(version)
     data = merger.merge_meta_into_sonix(input_dir,target_filename)
     converter.convert_sonix_data(data,language,category)
 
 """processes a PRRAT grid (excel file) into the ProductWords data base table."""
 def process_praat(input_filename, language,category,version):
-    init_DB()
-    DynamoDB.version = version
+    init_DB(version)
     converter.convert_prrat(input_filename,language,category)
 
 """make the following changes:
@@ -73,8 +72,7 @@ def process_praat(input_filename, language,category,version):
 - add the transform 
 """ 
 def add_transform(word,transform,category,language,version):
-    init_DB()
-    DynamoDB.version = version
+    init_DB(version)
     converter.handleAddTransform(word,transform,language,category)
 
 """make the following changes:
@@ -83,10 +81,40 @@ def add_transform(word,transform,category,language,version):
 - apply this change to products (existing product words change: transform-->rawWord, and  index change accordingly
 """
 def remove_transform(word,language,category,version):
-    init_DB()
-    DynamoDB.version = version
+    init_DB(version)
     converter.handleRemoveTransform(word,language,category)
 
+"""updates index table in data base, from  the given tranforms in excel source file."""
+def add_transforms(source_filename,version):
+    init_DB(version)
+    converter.handleAddTransforms(source_filename)
+
+"""delete all product words asociated with filename (doesnt delete from index)"""
+def delete_product_words(filename,version):
+    init_DB(version)
+    converter.handleDeleteProductWords(filename,DynamoDB().get_table("productWords"))
+
+"""export to excell, all product words asociated with filename """
+def get_product_words(filename,target_filename,version):
+    init_DB(version)
+    items = converter.handleGetProductWords(filename)
+    update_sheet(target_filename,items)
+
+"""export to excell, all product words asociated with filename """
+def get_product_words_by_category(category,language,target_filename,version):
+    init_DB(version)
+    items = converter.handleGetProductWordsByCategory(category,language)
+    update_sheet(target_filename,items)
+    
+""" deletes word from index and all occurrences of the word in productWords."""
+def delete_from_index(word,language,category,version):
+    init_DB(version)
+    converter.handle_delete_from_index(word,language,category)
+
+"""processes a directory of PRRAT grids (excel file) into the ProductWords data base table."""
+def process_praat_dir(input_dir, language,category,version):
+    init_DB(version)
+    converter.process_praat_dir(input_dir,language,category)
 
 
 def listChunker(lst, csize:int):
