40a6d77797ed9a6dc5858a0bad219db096207273
shahar halutz <shahar.halutz@gmail.com>
2021-11-25 11:55:39 +0100

    export amarilData

--- amariltb/__init__.py
+++ amariltb/__init__.py
@@ -3,5 +3,7 @@
 
 from .decorators import singleton
 from .functions import listChunker, report, weirdCase
-
+from .amaril_data_filters import E_FilterType,FilterPWWordCount,FilterDiagnoses,FilterLanguage,FilterCategory,FilterNumberInRange,FilterStringEquals,FilterBool
+from .amaril_data_normelizer import E_Diagnosis 
+from .amaril_data import AmarilData
 
